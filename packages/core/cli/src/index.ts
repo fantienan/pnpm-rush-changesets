299e5c2f7a437c7194384a7d7eebc53d81190419
fantienan <363982607@qq,com>
2022-01-26 11:10:24 +0800

    fix: 测试

--- packages/core/cli/src/index.ts
+++ packages/core/cli/src/index.ts
@@ -2,7 +2,7 @@ import { log } from '@forestar/utils';
 import type { CoreParams } from './types';
 
 const core = (params?: CoreParams) => {
-  log.info('__filename1', __filename);
+  log.info('__filename', __filename);
 };
 
 export default core;
